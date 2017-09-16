133260a7d79c118451c5e5a39fb831ed6c1b179b
Hadjimina <philipp.hadjimina@gmx.ch>
2017-09-16 02:40:11 +0200

    fixed (merge) errors

--- app/src/main/java/com/example/philipp/fundmyshit/Activities/LoginActivity.java
+++ app/src/main/java/com/example/philipp/fundmyshit/Activities/LoginActivity.java
@@ -1,22 +1,21 @@
 package com.example.philipp.fundmyshit.Activities;
 
         import android.content.Context;
-        import android.content.Intent;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.preference.PreferenceManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-        import com.example.philipp.fundmyshit.R;
+import com.example.philipp.fundmyshit.R;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button signup,skip,login;
+    private Button signup,login;
     private TextView loginText, errorEmail, errorPassword;
     private EditText email, password;
     private String stringEmail, stringPassword;
@@ -38,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
 
         login = (Button) findViewById(R.id.login);
         signup = (Button) findViewById(R.id.signup);
-        skip = (Button) findViewById(R.id.skip);
+
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,13 +45,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), SignupActivity.class));
             }
         });
-
-        skip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(v.getContext(), MainActivity.class));
-            }
-        });
+        
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
