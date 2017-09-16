3fcf6b57b94dd56dc5e0af07e8401a31b25dd05e
David <davidn@student.ethz.ch>
2017-09-16 21:23:02 +0200

    right buttons

--- app/src/main/java/com/example/philipp/fundmyshit/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/philipp/fundmyshit/Activities/SettingsActivity.java
@@ -2,11 +2,6 @@ package com.example.philipp.fundmyshit.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-<<<<<<< HEAD
-import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
-=======
->>>>>>> e340dd161a16e97de43e6a833473ca90bd61dafd
 
 import com.example.philipp.fundmyshit.R;
 
@@ -19,29 +14,9 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-<<<<<<< HEAD
-
-=======
-/*
-        CheckBox beer = (CheckBox)findViewById(R.id.checkBox);
->>>>>>> e340dd161a16e97de43e6a833473ca90bd61dafd
-
-        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myChildToolbar);
-        ActionBar ab = getSupportActionBar();
-        TextView name = (TextView) findViewById(R.id.username);
-<<<<<<< HEAD
-
-=======
-        name.setText("Matt");
->>>>>>> e340dd161a16e97de43e6a833473ca90bd61dafd
-        ab.setDisplayHomeAsUpEnabled(true);
-
-
-        TextView balance = (TextView) findViewById(R.id.balance);
-        balance.setText(100);
-
-
-
     }
 }
+
+
+
+
